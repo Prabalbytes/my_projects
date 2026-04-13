fa58664768f7886bccc9a5302a3cf3b737c17bd4
Prabal das <prabaldas421@gmail.com>
2026-04-13 08:41:52 +0530

    fix case issue

--- server.js
+++ server.js
@@ -75,6 +75,8 @@ app.post("/contact", async (req, res) => {
 });
 
 // Server start
-app.listen(process.env.PORT, () => {
-  console.log(`🚀 Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
+
